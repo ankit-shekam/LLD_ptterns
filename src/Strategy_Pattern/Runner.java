f87bc77178316b8206b267fb966832855756a8a8
Ankit Shekam <60414930+ankit-shekam@users.noreply.github.com>
2025-01-19 19:54:34 +0530

    strategy pattern

--- src/Strategy_Pattern/Runner.java
+++ src/Strategy_Pattern/Runner.java
@@ -7,6 +7,9 @@ public class Runner {
 
   public static void main(String[] args) {
 
+//    create a vehicle package where each vehicle has a specific type of drive() logic and you
+//    have the capability of switching between the different drive logic with minimal change
+    
     Vehicle v1 = new Vehicle(new NormalDrive());
     v1.drive(v1.hashCode());
 
